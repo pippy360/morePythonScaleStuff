53789f3c1d85de1105d611f1ad7fa8a9ec28460f
Tom <tomnomnom1@gmail.com>
2016-08-31 15:31:36 +0100

    added more stuff to docs

--- newkeypointFinder.py
+++ newkeypointFinder.py
@@ -18,6 +18,8 @@ import jsonHandling as jh
 import os
 
 
+g_pixelVals = [16, 124, 115, 68, 98, 176, 225, 55, 50, 53, 129, 19, 57, 160, 143, 237, 75, 164, 206, 167, 103, 140, 90, 112, 244, 240, 107, 202, 185, 72, 71, 109, 74, 183, 205, 46, 121, 180, 142, 126, 38, 247, 166, 144, 67, 134, 194, 198, 23, 186, 33, 163, 24, 117, 37, 76, 147, 47, 52, 42, 70, 108, 30, 54, 89, 59, 73, 91, 151, 6, 173, 86, 182, 178, 10, 207, 171, 13, 77, 88, 159, 125, 11, 188, 238, 41, 92, 118, 201, 132, 48, 28, 195, 17, 119, 64, 25, 45, 114, 80, 187, 105, 204, 158, 20, 169, 83, 191, 199, 234, 136, 81, 252, 141, 242, 219, 138, 161, 154, 135, 63, 153, 239, 130, 223, 249, 122, 93, 216, 127, 111, 15, 12, 8, 44, 193, 245, 0, 235, 120, 31, 165, 3, 155, 43, 26, 152, 94, 29, 232, 35, 218, 230, 233, 214, 217, 7, 156, 189, 228, 137, 209, 145, 226, 97, 215, 170, 51, 224, 100, 61, 69, 250, 4, 34, 56, 255, 60, 84, 110, 203, 222, 133, 248, 106, 212, 87, 253, 208, 101, 116, 251, 190, 99, 32, 113, 157, 27, 79, 82, 146, 149, 5, 210, 65, 22, 181, 131, 62, 36, 184, 196, 231, 192, 66, 213, 2, 254, 174, 211, 236, 229, 58, 221, 21, 150, 123, 175, 177, 179, 246, 96, 227, 1, 18, 241, 49, 128, 78, 40, 14, 162, 85, 39, 172, 104, 9, 200, 220, 139, 168, 95, 243, 197, 148, 102]
+
 def getTheKeypoints(img):
 	img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 	ret,img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
@@ -82,22 +84,70 @@ def main(imgName):
 	
 	return img2
 
+def recolour(imgName):
+	newg_pixelVals = g_pixelVals
+	div = 40
+	for i in range(len(g_pixelVals)/div):
+		for j in range(div):
+			newg_pixelVals[i*div + j] = newg_pixelVals[i*div]
+
+		finalCount = (i*div) + div
 
 
-imgName1 = 'lenna_big1.jpg'
-imgName2 = 'lenna_big2.jpg'
-imgName3 = 'lenna_big3.jpg'
-imgName4 = 'lenna_big4.jpg'
-finImg1 = main("./input/"+ imgName1 +"")
-finImg2 = main("./input/"+ imgName2 +"")
-finImg3 = main("./input/"+ imgName3 +"")
-finImg4 = main("./input/"+ imgName4 +"")
+	for i in range( len(g_pixelVals) - finalCount ):
+		newg_pixelVals[ len(g_pixelVals) -1 - i ] = newg_pixelVals[ finalCount ]
 
-cv2.imwrite('t1.jpg', finImg1)
-cv2.imwrite('t2.jpg', finImg2)
-cv2.imwrite('t3.jpg', finImg3)
-cv2.imwrite('t4.jpg', finImg4)
+
+	print finalCount
+	print newg_pixelVals
+
+	img2 = cv2.imread(imgName)
+	img  = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+
+	height, width= img.shape
+	for i in range(0, height):             #looping at python speed...
+		for j in range(0, width):     #...
+			val = img[i,j]
+			val = newg_pixelVals[val]
+
+			if val%3 == 0:
+				threeVal = (0,0,val)
+			elif val%3 == 1:
+				threeVal = (0,val,0)
+			else:
+				threeVal = (val,0,0)
+
+			img2[i,j] = threeVal
+
+
+	cv2.imwrite(imgName + 'lenna_big_diff_cols.png', img2)
+	#cv2.waitKey()
+
+
+imgName1 = 'small_lenna1.jpg'
+imgName2 = 'small_lenna2.jpg'
+imgName3 = 'small_lenna3.jpg'
+imgName4 = 'small_lenna4.jpg'
+
+
+recolour("./input/"+ imgName1 +"")
+recolour("./input/"+ imgName2 +"")
+recolour("./input/"+ imgName3 +"")
+recolour("./input/"+ imgName4 +"")
+
+
+
+#finImg1 = main("./input/"+ imgName1 +"")
+#finImg2 = main("./input/"+ imgName2 +"")
+#finImg3 = main("./input/"+ imgName3 +"")
+#finImg4 = main("./input/"+ imgName4 +"")
+#
+#cv2.imwrite('t1.jpg', finImg1)
+#cv2.imwrite('t2.jpg', finImg2)
+#cv2.imwrite('t3.jpg', finImg3)
+#cv2.imwrite('t4.jpg', finImg4)
 #cv2.waitKey()
+
 
 
 
