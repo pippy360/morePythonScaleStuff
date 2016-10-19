5a3fe6d807d1028ead949048c28ea44b48f48814
pippy360 <tomnomnom1@gmail.com>
2016-10-19 15:38:40 +0100

    refactorig

--- keypoints.py
+++ keypoints.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import time
 
 import matplotlib.pyplot as plt
@@ -19,9 +19,15 @@ import sys
 from scipy import signal
 from scipy.signal import argrelextrema
 import new_shapes as ns
+import plotting
 ####### gen points ########
 
 g_name = 'something'
+g_enable_plotting = True
+
+#pts = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0)]
+#pts = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)]
+#pts = [(0,0**2),(1,1**2),(2,2**2),(3,3**2),(4,4**2),(5,5**2),(6,6**2),(7,7**2),(8,8**2),(9,9**2),(10,10**2)]
 
 def PointsInCircum(r,n=100):
     return [(math.cos(2*pi/n*x)*r,math.sin(2*pi/n*x)*r) for x in xrange(0,n+1)]
@@ -44,8 +50,99 @@ def thatCurve(a=9, b=6, noOfPoints=200):
 		ret.append( (x[i], y[i]) )
 	return ret
 
+#####################################
+####### plots points ########
+#####################################
+def plotOneFuncVsOrgPoints(tList, fx_t, org_x):
+	if not g_enable_plotting:
+		return 
+	#PLOT 
+	plot(tList, org_x, 'x', color="red")
+	plot(tList, fx_t(tList), 'b', color="blue")
+	show()
+	#PLOT
+
+def plotTwoFuncsVsOrgPoints(tList, fx_t, org_x, fy_t, org_y):
+	if not g_enable_plotting:
+		return 
+	#PLOT 
+	subplot(211)
+	plot(tList, org_x, 'x', color="red")
+	plot(tList, fx_t(tList), 'b', color="blue")
+	subplot(212)
+	plot(tList, org_y, 'x', color="red")
+	plot(tList, fy_t(tList), 'b', color="blue")
+	#plot(fx_t(tList), fy_t(tList), 'b', color="blue")
+	show()
+	#PLOT
+
+def randomPlot1(org_x, org_y, arcLengthList, fx_s, fy_s):
+	#PLOT 
+	subplot(411)
+	plot(org_x, org_y, 'b', color="blue")
+	subplot(412)
+	plot(arcLengthList, org_x, 'x', color="red")
+	plot(arcLengthList, fx_s(arcLengthList), 'b', color="blue")
+	subplot(413)
+	plot(arcLengthList, org_y, 'x', color="red")
+	plot(arcLengthList, fy_s(arcLengthList), 'b', color="blue")
+	subplot(414)
+	plot(fx_s(arcLengthList), fy_s(arcLengthList), 'b', color="blue")
+	show()
+	#PLOT
 	
+#####################################
+####### debug prints #######
+#####################################
+def printTheDerivativesError(x_, y_):
+	print "x_**2 + y_**2"
+	vals = x_**2 + y_**2
+	print vals
+	print "total error: " + str(np.sum(abs(vals-1)))
+	
+def randomPrint1(arcLengthList, org_x, org_y, fx_s, fy_s):
+	print 'now the functions'
+	print arcLengthList
+	print org_x
+	print org_y
+	print fx_s(arcLengthList)
+	print fy_s(arcLengthList)	
+
+#####################################
+####### make less points
+#####################################
+def breakUpFullLengthOfArcIntoXPoints(fullLength, noOfPoints, addZeroPoint=False):
+	step = float(fullLength)/float(noOfPoints)
+	ret = []
+	tempVal = 0
+	if addZeroPoint:
+		ret.append(0)
+
+	for i in range(noOfPoints):
+		tempVal += step
+		ret.append(tempVal)
+		
+	return ret
+
+#express the function in less points by parameterizing WRT some variable (t) 
+#and then interpolating
+def getSimplePts(pts, maxNoOfPoints=100):
+	org_x, org_y = pts[:, 0], pts[:, 1]
+	tList = np.arange(org_x.shape[0])
+	s_no = 1.01
+	fx_t = UnivariateSpline(tList, org_x, k=3, s=s_no)
+	fy_t = UnivariateSpline(tList, org_y, k=3, s=s_no)
+	newTList = breakUpFullLengthOfArcIntoXPoints(tList[-1], maxNoOfPoints, addZeroPoint=True)
+	xt = fx_t(newTList)
+	yt = fy_t(newTList)
+	return xt, yt, newTList
+#####################################
+####### \make less points
+#####################################
+
+#####################################
 ####### main code ########	
+#####################################
 
 def getPointsAndFirstDerAtT(t, fx, fy):
 	return fx([t])[0], fx.derivative(1)([t])[0], fy([t])[0], fy.derivative(1)([t])[0]
@@ -79,9 +176,55 @@ def getTheValueOfTAtPosition(s, pts):
 	return res
 ##----	
 
-def convertTListToArcLengthList(tList, fx, fy):
+def convertTListToArcLengthList(tList, fx_t, fy_t):
+ 	print "starting the integral"
+	time1 = time.time()
+	arcLengthList = _convertTListToArcLengthList(tList, fx_t, fy_t)
+	time2 = time.time()
+	print 'function took %0.3f seconds' % ((time2-time1))
+	return arcLengthList
+
+	
+def getParameterizedFunctionFromPoints(tList, x_pts, y_pts, smoothing=None):
+	fx_t = UnivariateSpline(tList, x_pts, k=3, s=smoothing)
+	fy_t = UnivariateSpline(tList, y_pts, k=3, s=smoothing)
+	return fx_t, fy_t
+	
+#if reparameterizing with respect to arc length then [s1, s2, ...] = t_to_s_function([t1, t2, ...])
+def reParameterizeFunctionFromPoints(t_to_s_function, tList, fx_t, fy_t, smoothing=None):
+	#for each point (org_x[i], org_y[i]) the "arcLengthList" gives use the arc length from 0 to that point
+	arcLengthList = t_to_s_function(tList, fx_t, fy_t)
+	
+	#CAREFUL!!! we might use fx_t(tList) here, or x_org 
+	#but only if that's what the arcLengthListIsDefinedAS ??? it must be, how else would we make the spline????
+	fx_s, fy_s = getParameterizedFunctionFromPoints(arcLengthList, fx_t(tList), fy_t(tList), smoothing=smoothing)
+	return arcLengthList, fx_s, fy_s 
+	
+def _convertTListToArcLengthList(tList, fx, fy):
 	return TtoS(tList, fx, fy)
 
+def getFirstAndSecondDerivForTPoints(arcLengthList, fx_s, fy_s):
+	x = fx_s(arcLengthList)
+	x_ = fx_s.derivative(1)(arcLengthList)
+	x__ = fx_s.derivative(2)(arcLengthList)
+
+	y = fy_s(arcLengthList)
+	y_ = fy_s.derivative(1)(arcLengthList)
+	y__ = fy_s.derivative(2)(arcLengthList)
+	return x, x_, x__, y, y_, y__
+	
+def newArcLengthList(oldArcLengthList):
+	return oldArcLengthList
+	
+#Remember: curvature points will be the input points (and so won't be equidistant if the arcLengthList isn't)
+def getCurvatureForPoints(arcLengthList, fx_s, fy_s, smoothing=None):
+	x, x_, x__, y, y_, y__ = getFirstAndSecondDerivForTPoints(arcLengthList, fx_s, fy_s)
+	curvature = abs(x_* y__ - y_* x__) / np.power(x_** 2 + y_** 2, 3 / 2)
+	fCurvature = UnivariateSpline(arcLengthList, curvature, s=smoothing)
+	dxcurvature = fCurvature.derivative(1)(arcLengthList)
+	dx2curvature = fCurvature.derivative(2)(arcLengthList)
+	return curvature, dxcurvature, dx2curvature
+	
 def parameterizeFunctionWRTArcLength(pts):
 	org_x, org_y = pts[:, 0], pts[:, 1]
 	return _parameterizeFunctionWRTArcLength(org_x, org_y)
@@ -89,199 +232,54 @@ def parameterizeFunctionWRTArcLength(pts):
 def _parameterizeFunctionWRTArcLength(org_x, org_y):
 		
 	tList = np.arange(org_x.shape[0])
-	s_no = 100.0
-	fx_t = UnivariateSpline(tList, org_x, k=3, s=s_no)
-	fy_t = UnivariateSpline(tList, org_y, k=3, s=s_no)
+	fx_t, fy_t = getParameterizedFunctionFromPoints(tList, org_x, org_y, smoothing=1.0)
 
 	#PLOT 
-	subplot(211)
-	plot(tList, org_x, 'x', color="red")
-	plot(tList, fx_t(tList), 'b', color="blue")
-	subplot(212)
-	plot(tList, org_y, 'x', color="red")
-	plot(tList, fy_t(tList), 'b', color="blue")
-	#plot(fx_t(tList), fy_t(tList), 'b', color="blue")
-	show()
+	#plotTwoFuncsVsOrgPoints(tList, fx_t, org_x, fy_t, org_y)
 	#PLOT
 
-	#start param for t
-
-	#for each point (org_x[i], org_y[i]) the "arcLengthList" gives use the arc length from 0 to that point
-	print "starting the integral"
-	time1 = time.time()
-	arcLengthList = convertTListToArcLengthList(tList, fx_t, fy_t)
-	time2 = time.time()
-	print 'function took %0.3f ms' % ((time2-time1)*1000.0)
-
-	#CAREFUL!!! we might use fx_t(tList) here, or x_org 
-	#but only if that's what the arcLengthListIsDefinedAS ??? it must be, how else would we make the spline????
-	fx_s = UnivariateSpline(arcLengthList, fx_t(tList), s=None)
-	fy_s = UnivariateSpline(arcLengthList, fy_t(tList), s=None)
-
-	print 'now the functions'
-	print arcLengthList
-	print org_x
-	print org_y
-	print fx_s(arcLengthList)
-	print fy_s(arcLengthList)
+	arcLengthList, fx_s, fy_s = reParameterizeFunctionFromPoints(convertTListToArcLengthList, tList, fx_t, fy_t, smoothing=1.0)
 	
-	#PLOT 
-	subplot(411)
-	plot(org_x, org_y, 'b', color="blue")
-	subplot(412)
-	plot(arcLengthList, org_x, 'x', color="red")
-	plot(arcLengthList, fx_s(arcLengthList), 'b', color="blue")
-	subplot(413)
-	plot(arcLengthList, org_y, 'x', color="red")
-	plot(arcLengthList, fy_s(arcLengthList), 'b', color="blue")
-	subplot(414)
-	plot(fx_s(arcLengthList), fy_s(arcLengthList), 'b', color="blue")
-	show()
-	#PLOT
-
+	#PRINT DEBUG
+	randomPrint1(arcLengthList, org_x, org_y, fx_s, fy_s)
+	randomPlot1(org_x, org_y, arcLengthList, fx_s, fy_s)
+	#PRINT DEBUG
 	
-#	x = arcLengthList
-	x_ = fx_s.derivative(1)(arcLengthList)
-	x__ = fx_s.derivative(2)(arcLengthList)
-#	y = 
-	y_ = fy_s.derivative(1)(arcLengthList)
-	y__ = fy_s.derivative(2)(arcLengthList)
+	arcLengthList = newArcLengthList(arcLengthList)
+	x, x_, x__, y, y_, y__ = getFirstAndSecondDerivForTPoints(arcLengthList, fx_s, fy_s)
 
-	curvature = abs(x_* y__ - y_* x__) / np.power(x_** 2 + y_** 2, 3 / 2)
+	#PRINT DEBUG
+	printTheDerivativesError(x_, y_)
+	#PRINT DEBUG
+	
+	curvature, dxcurvature, dx2curvature = getCurvatureForPoints(arcLengthList, fx_s, fy_s, smoothing=1.0)
 
-	print "x_**2 + y_**2"
-	vals = x_**2 + y_**2
-	print vals
-	print np.sum(abs(vals-1))
-
-	dxcurvature = UnivariateSpline(arcLengthList, curvature, s=s_no).derivative(1)(arcLengthList)
-	dx2curvature = UnivariateSpline(arcLengthList, curvature, s=s_no).derivative(2)(arcLengthList)
 	return org_x, org_y, x_, y_, x__, y__, arcLengthList, curvature, dxcurvature, dx2curvature, arcLengthList[-1]
-
-def plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, idx, fullLength_s):
-	fullLen = len(s)
-	i = idx
-
-	
-	subplot(321)
-	#axis([0.0,20.0,-15.0,15.0])
-#	pylab.axis([0.0,350.0,0.0,100.0])
-	#pylab.xlim([0,fullLength_s])
-	pylab.axhline(0, color='black')
-	pylab.plot(s[0:i+1], curvature[0:i+1], 'b', color="red")
-	
-	
-	ax = subplot(322)
-	#axis([-1.5,1.5,-1.5,1.5])
-	#pylab.xlim([0,fullLength_s])
-	pylab.plot(xs[0:i+1], ys[0:i+1], 'b', linewidth=2, color="red")
-
-	pylab.plot(xs[i:fullLen], ys[i:fullLen], 'b', color="grey")
-	ax = ax.axes
-	div = 1#1/(math.sqrt(dxdt[0:i+1][-1]**2 + dydt[0:i+1][-1]**2))
-	#div = div * 0.5
-	ax.arrow(xs[0:i+1][-1], ys[0:i+1][-1], dxdt[0:i+1][-1]*div, dydt[0:i+1][-1]*div, head_width=0.05, head_length=0.1, fc='r', ec='r')
-	div = 1#1/(math.sqrt((dxdt[0:i+1][-1]+d2xdt[0:i+1][-1])**2 + (dydt[0:i+1][-1]+d2ydt[0:i+1][-1])**2))
-	#div = div * 0.5
-	ax.arrow(xs[0:i+1][-1], ys[0:i+1][-1], (dxdt[0:i+1][-1]+d2xdt[0:i+1][-1])*div, ((dydt[0:i+1][-1]+d2ydt[0:i+1][-1]))*div, head_width=0.05, head_length=0.1, fc='b', ec='b')
-
-	subplot(323)
-	#axis([0.0,20.0,-1.0,1.0])
-	#pylab.xlim([0,fullLength_s])
-	pylab.axhline(0, color='black')
-	pylab.plot(s[0:i+1], dxdt[0:i+1], 'b', linewidth=2, color="red")
-	subplot(324)
-	#axis([0.0,20.0,-1.0,1.0])
-	pylab.axhline(0, color='black')
-	pylab.plot(s[0:i+1], dydt[0:i+1], 'b', linewidth=2, color="red")
-	
-	subplot(325)
-	#axis([0.0,20.0,-2.0,2.0])
-	#pylab.xlim([0,fullLength_s])
-	pylab.axhline(0, color='black')
-	vals = dydt[0:i+1]**2+dxdt[0:i+1]**2
-	vals = abs(vals-1)
-	pylab.plot(s[0:i+1], abs(dx2curvature[0:i+1]), 'b', linewidth=2, color="red")
-
-	subplot(326)
-	#axis([0.0,20.0,-10.0,10.0])
-	#pylab.xlim([0,fullLength_s])
-	pylab.axhline(0, color='black')
-	#pylab.plot(s[0:i+1], d2ydt[0:i+1], 'b', linewidth=2, color="red")
-	tempVals = abs(dxcurvature[0:i+1])
-	#tempVals = np.log(tempVals)
-	pylab.plot(s[0:i+1], (dxdt**2 + dydt**2)[0:i+1], 'b', linewidth=2, color="red")
-	print "vals[-1]"
-	print vals[-1]
-	pylab.savefig('output_debug/'+g_name+'_foo'+str(idx)+'.png')
-	pylab.clf()
-
 
 def genImages2(retX, retY):
 	return genImages
-	
-####### make less points
 
-def breakUpFullLengthOfArcIntoXPoints(fullLength, noOfPoints, addZeroPoint=False):
-	step = float(fullLength)/float(noOfPoints)
-	ret = []
-	tempVal = 0
-	if addZeroPoint:
-		ret.append(0)
-
-	for i in range(noOfPoints):
-		tempVal += step
-		ret.append(tempVal)
-		
-	return ret
-
-	
-#express the function in less points by parameterizing WRT some variable (t) 
-#and then interpolating
-def getSimplePts(pts, maxNoOfPoints=100):
-	org_x, org_y = pts[:, 0], pts[:, 1]
-	tList = np.arange(org_x.shape[0])
-	s_no = 1.01
-	fx_t = UnivariateSpline(tList, org_x, k=3, s=s_no)
-	fy_t = UnivariateSpline(tList, org_y, k=3, s=s_no)
-	newTList = breakUpFullLengthOfArcIntoXPoints(tList[-1], maxNoOfPoints, addZeroPoint=True)
-	xt = fx_t(newTList)
-	yt = fy_t(newTList)
-	return xt, yt, newTList
-
-
-
-####### \make less points
-
-
-
-def genImagesWithDisplayFix(pts, numberOfPixelsPerUnit=25):
+def genImagesWithDisplayFix(pts, numberOfPixelsPerUnit=1):
 	org_x, org_y = pts[:, 0], pts[:, 1]
 	org_y = org_y[310:500]
 	org_x = org_x[310:500]
+
 	#org_x, org_y, junk = getSimplePts(pts)
-	#org_x = np.multiply(org_x, 1./float(numberOfPixelsPerUnit))
-	#org_y = np.multiply(org_y, 1./float(numberOfPixelsPerUnit))
+	org_x = np.multiply(org_x, 1./float(numberOfPixelsPerUnit))
+	org_y = np.multiply(org_y, 1./float(numberOfPixelsPerUnit))
 	
-	print 'mult done'
+	print 'Vals of org_x and org_y after mult'
 	print org_x
 	print org_y
-	print 'mult done end'
 	
-	xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, fullLength_s = _parameterizeFunctionWRTArcLength(org_x, org_y)
-
-	#PLOT
-	#subplot(121)
-	#plot(s, ys)
-	#subplot(122)
-	#plot(s, xs)
-	#show()
-	#PLOT
+	xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, fullLength_s =_parameterizeFunctionWRTArcLength(org_x, org_y)
 
 	#for i in range(len(dx2curvature)):
-	#	plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, i, fullLength_s)
+	#	plotting.plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, i, fullLength_s)
 
-
+	
+def getEqidistantPointsAlongFunction(pts):
+	pass
 
 
 def genImages(pts):
@@ -290,15 +288,7 @@ def genImages(pts):
 
 	org_x, org_y = new_org_x, new_org_y#pts[:, 0], pts[:, 1]
 
-
 	xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, fullLength_s = _parameterizeFunctionWRTArcLength(org_x, org_y)
-
-	#PLOT
-	#plot(pts[:, 0], pts[:, 1], ':')
-	#plot(xs, ys, 'x', color='red')
-	#plot(new_org_x, new_org_y, 'b', color='blue')
-	#show()
-	#PLOT
 
 	dx2curvature = abs(dx2curvature)
 	maxm = argrelextrema(dx2curvature, np.greater)  # (array([1, 3, 6]),)
@@ -307,7 +297,7 @@ def genImages(pts):
 	print maxm
 
 	#for i in range(len(dx2curvature)):
-	#	plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, i, fullLength_s)
+	#	plotting.plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, i, fullLength_s)
 
 
 	coordsx = []
@@ -325,12 +315,9 @@ def genImages(pts):
 	#for i in range(len(s)):
 	#	#pylab.plot(xs, ys, 'b', color="blue")
 	#	pylab.axhline(0, color='black')
-	#	plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, i)
+	#	plotting.plotItAtIndex(xs, ys, dxdt, dydt, d2xdt, d2ydt, s, curvature, dxcurvature, dx2curvature, i)
 	
 
-#pts = [(0,0),(1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),(9,0),(10,0)]
-#pts = [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10)]
-#pts = [(0,0**2),(1,1**2),(2,2**2),(3,3**2),(4,4**2),(5,5**2),(6,6**2),(7,7**2),(8,8**2),(9,9**2),(10,10**2)]
 
 
 #pts = PointsInCircum(50,8)
